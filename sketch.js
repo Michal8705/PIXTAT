3013b8e2a3d1e31207c09af8981594d7f3b9d388
Michal8705 <62724357+Michal8705@users.noreply.github.com>
2023-11-10 17:38:40 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ var choiceSter2 = 0;
 var choiceTimer1 = 0;
 var choiceTimer2 = 0;
 var lastChoiceSter = 100;
-var mobileDevice = 1;
+var mobileDevice = 0;
 var buttonH = 50;
 var extraButtonH = 0;
 var lastExtraButtonH = 0;
@@ -878,7 +878,7 @@ function draw() {
   stroke(255,0,0,255)
   text(test1,100,100)  
   text(chartPanelStart,100,130)  
-  text(firstRun+200,100,150)  
+  text(firstRun+300,100,150)  
   
 
  if ((chartPlayController != -1 && 
